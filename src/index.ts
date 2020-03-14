0cbe7c0852f5a3dd8bf5de8cdeb2cd00f2589fac
Yoni Calsin <yonicb20@gmail.com>
2020-03-14 06:55:13 -0500

    docs: ✏️ Aggregated header comment !"

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,10 @@
+/*!
+ * Merge Options Default v1.0.9 (https://github.com/yoicalsin/merge-options-default)
+ * Copyright 2020 The Merge Options Default Authors
+ * Copyright 2020 Yoni Calsin.
+ * Licensed under MIT (https://github.com/yoicalsin/merge-options-default/blob/master/LICENSE)
+ */
+
 export const Merge = (...moreOptions: any[]) => {
    let payload = {};
    for (const v of moreOptions) {
